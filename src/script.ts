910aa5873edb658cd1907a9b98863f10cedbc590
Benj <benj>
2023-06-08 13:00:31 +0100

    Add banner when placing order

--- src/script.ts
+++ src/script.ts
@@ -27,6 +27,8 @@ async function checkOrder(logger: (message: string) => void) {
     throw new Error(`Found 0 matching stage for matching takeaway info for this URL`);
   }
 
+  addBanner();
+
   logger(`Retrieving order from storage`);
 
   // Retrieve the order from storage
@@ -48,6 +50,70 @@ async function checkOrder(logger: (message: string) => void) {
 
   // Save any updates made to the order
   await chrome.storage.sync.set({ order: JSON.stringify(order) });
+}
+
+// Adds a banner to the page, if not already
+function addBanner() {
+  const BANNER_ID = "foodie-fastlane-banner";
+  const existingBanner = document.querySelector<HTMLDivElement>(`#${BANNER_ID}`);
+
+  if (existingBanner) {
+    return;
+  }
+
+  const banner = document.createElement("div");
+  banner.id = BANNER_ID;
+
+  const title = document.createElement("h4");
+  title.className = "title";
+  title.textContent = "Foodie Fastlane";
+  banner.appendChild(title);
+
+  const message = document.createElement("h5");
+  message.className = "message";
+  message.textContent = "Your order is being processed, please wait";
+  banner.appendChild(message);
+
+  const styles = document.createElement("style");
+  styles.textContent= `  
+    body {
+      margin-top: 36px !important;
+    }
+    
+    #foodie-fastlane-banner {
+      display: flex !important;
+      align-items: center !important;
+      justify-content: center !important;
+      position: fixed !important;
+      left: 0 !important;
+      right: 0 !important;
+      bottom: 0 !important;
+      z-index: 9999999 !important;
+      height: 36px !important;
+      background-color: #df7503 !important;
+      color: #eee !important;
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif !important;
+    }
+    
+    #foodie-fastlane-banner .title,
+    #foodie-fastlane-banner .message {
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif !important;
+      font-size: 14px !important;
+      color: white !important;
+      margin: 0 !important;
+      padding: 0 !important;
+      margin-right: 0.5em !important;
+      font-weight: normal !important;
+      text-transform: uppercase !important;
+    }
+
+    #foodie-fastlane-banner .title {
+      font-weight: bold !important;
+    }
+  `;
+  banner.appendChild(styles);
+
+  document.body.insertAdjacentHTML("afterbegin", banner.outerHTML);
 }
 
 const logger = (message: string) => {
