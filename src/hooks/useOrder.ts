da65e390e3891ae82237066ce3e6da682cd1a5fc
Benj <benj>
2023-06-09 10:59:50 +0100

    Fix order saving

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,7 +4,7 @@ import { TakeawayOrder } from "../data";
 function useOrder(order: TakeawayOrder) {
   useEffect(() => {
     chrome.storage.sync.set({ order: JSON.stringify(order) });
-  }, []);
+  }, [order]);
 }
 
 export default useOrder;
